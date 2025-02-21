e18b58acdc613ff1135f593c63f656f69c47fd64
Vladislav Korenev <korenevlad17@yandex.ru>
2025-02-21 16:52:47 +0300

    [CSJHW-1]: fix reading file

--- week-1/OrderReportCreator/Application/Senders/FileOrderReportSender.cs
+++ week-1/OrderReportCreator/Application/Senders/FileOrderReportSender.cs
@@ -5,19 +5,20 @@ using OrderReportCreator.Domain.Models;
 namespace OrderReportCreator.Application.Senders;
 public class FileOrderReportSender: IOrderReportSender
 {
-    private const string DirPath = @"..\..\..\..\ReportFiles";
+    private const string DirPath = "ReportFiles";
     public bool CanSendReport(ResponseFormat responseFormat) 
         => responseFormat == ResponseFormat.File;
     public string SendReport(Report report)
     {
+        var fullPathDir = GetPathDir();
         try
         {
-            if (!Directory.Exists(DirPath))
+            if (!Directory.Exists(fullPathDir))
             {
-                Directory.CreateDirectory(DirPath);
+                Directory.CreateDirectory(fullPathDir);
             }
             var fileName = $"Report-{DateTime.Now:yyyyMMdd_HHmmss}.csv";
-            var filePath = Path.Combine(DirPath, fileName);
+            var filePath = Path.Combine(fullPathDir, fileName);
             using (var writer = new StreamWriter(filePath))
             using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
             {
@@ -30,5 +31,22 @@ public class FileOrderReportSender: IOrderReportSender
             Console.WriteLine(ex.Message);
             throw;
         }
+    }
+    private string GetProjectRoot()
+    {
+        var baseDirPath = new DirectoryInfo(AppContext.BaseDirectory);
+        while (baseDirPath != null && !File.Exists(Path.Combine(baseDirPath.FullName, $"{baseDirPath.Name}.csproj")))
+        {
+            baseDirPath = baseDirPath.Parent;
+        }
+        return baseDirPath?.FullName ?? throw new Exception("Не удалось найти корень проекта");
+    }
+
+    private string GetPathDir()
+    {
+        var projectRoot = GetProjectRoot();
+        string relativePathDir = Path.Combine(projectRoot, "..", DirPath);
+        string fullPathDir = Path.GetFullPath(relativePathDir);
+        return fullPathDir;
     }
 }
