ac162ea7377ef1a4bd5305556b1626a9617e5e02
Владислав Коренев <korenevlad17@gmail.com>
2025-03-26 14:22:03 +0300

    [CSJHW-6]: Init homework-6

---
+++ week-6/src/HomeworkApp.Dal/Entities/UserEntityV1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace HomeworkApp.Dal.Entities;
+
+public record UserEntityV1
+{
+    public required long Id { get; init; }
+
+    public required string Email { get; init; }
+
+    public required DateTimeOffset CreatedAt { get; init; }
+
+    public DateTimeOffset? BlockedAt { get; init; }
+}
