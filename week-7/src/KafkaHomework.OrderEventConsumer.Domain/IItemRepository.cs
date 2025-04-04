e2ffa282b84119faeb1304861cf1742b8b6e2ae4
Владислав Коренев <korenevlad17@gmail.com>
2025-04-04 21:06:03 +0300

    [CSJHW-7]: End main task

--- week-7/src/KafkaHomework.OrderEventConsumer.Domain/IItemRepository.cs
+++ week-7/src/KafkaHomework.OrderEventConsumer.Domain/IItemRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace KafkaHomework.OrderEventConsumer.Domain;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using KafkaHomework.OrderEventConsumer.Domain.Contracts;
 
+namespace KafkaHomework.OrderEventConsumer.Domain;
 public interface IItemRepository
 {
+    Task UpdateItemStatisticsAsync(long itemId, int quantity, OrderEvent.OrderStatus status, CancellationToken token);
 }
